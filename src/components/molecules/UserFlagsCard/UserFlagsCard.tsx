5a9867ee6e5b11a76c5ec4157490c9ef782f6d69
sotomaque <sotomaque@gmail.com>
2022-07-13 18:40:38 +0100

    cleanup user flags component

--- src/components/molecules/UserFlagsCard/UserFlagsCard.tsx
+++ src/components/molecules/UserFlagsCard/UserFlagsCard.tsx
@@ -1,12 +1,7 @@
-import { useState } from 'react';
 import { User } from 'types';
 
 const UserFlagsCard = ({ user }: { user: User }) => {
-  const [collapsed, setCollapsed] = useState(false);
-
-  const handleOnHide = () => {
-    setCollapsed((prev) => !prev);
-  };
+  const hasUserFlags = user.userFlags && user.userFlags.length > 0;
 
   return (
     <div className="mx-auto container bg-white shadow rounded overflow-x-scroll">
@@ -16,43 +11,47 @@ const UserFlagsCard = ({ user }: { user: User }) => {
         <h3 className="text-lg leading-6 font-medium text-gray-900">
           User Flags
         </h3>
-        <button
-          onClick={() => handleOnHide()}
-          className={`ml-0 sm:ml-6 transition duration-150 ease-in-out focus:outline-none rounded text-white px-5 h-8 flex items-center text-sm
-        ${
-          collapsed
-            ? 'bg-blue-700 hover:bg-blue-600'
-            : 'bg-indigo-700 hover:bg-indigo-600'
-        }
-        ${user.userFlags && user.userFlags.length > 1 ? '' : 'hidden'}
-        `}
-        >
-          {collapsed ? 'Expand' : 'Collapse'}
-        </button>
       </div>
       {/* Content  */}
       <div className="px-4">
-        {/* Flags */}
-        {user.userFlags &&
-          user.userFlags
-            .slice(0, collapsed ? 1 : user.userFlags.length)
-            .map((userFlag, idx) => {
-              const isLast = collapsed
-                ? true
-                : idx === (user.userFlags ? user.userFlags.length - 1 : -1);
-              return (
-                <div
-                  key={`userFlag-${userFlag}-${idx}`}
-                  className={`${idx === 0 && 'mt-2'} ${isLast && 'pb-2'}`}
-                >
-                  {idx != 0 && <hr className="my-2" />}
-                  <p>{userFlag}</p>
-                </div>
-              );
-            })}
+        {hasUserFlags ? (
+          <div>
+            {user!.userFlags!.map((userFlag, idx) => (
+              <UserFlagsContent
+                key={`userFlag-${userFlag}-${idx}`}
+                userFlag={userFlag}
+                idx={idx}
+                totalFlags={user.userFlags!.length}
+              />
+            ))}
+          </div>
+        ) : (
+          <NoFlagsPlaceholder user={user} />
+        )}
       </div>
     </div>
   );
 };
 
 export default UserFlagsCard;
+
+const UserFlagsContent = ({
+  userFlag,
+  idx,
+  totalFlags,
+}: {
+  userFlag: string;
+  idx: number;
+  totalFlags: number;
+}) => (
+  <div className={`${idx === 0 && 'mt-2'} ${idx === totalFlags - 1 && 'pb-2'}`}>
+    {idx != 0 && <hr className="my-2" />}
+    <p>{userFlag}</p>
+  </div>
+);
+
+const NoFlagsPlaceholder = ({ user }: { user: User }) => (
+  <div className="p4">
+    <p>No Flags for User: {user.userID}</p>
+  </div>
+);
