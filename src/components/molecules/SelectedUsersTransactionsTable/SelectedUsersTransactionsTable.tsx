ffcc9ac47d1a269b85d3a189f1575627cd7e5a50
sotomaque <sotomaque@gmail.com>
2022-07-23 11:44:46 -1000

    referrals dark mode

--- src/components/molecules/SelectedUsersTransactionsTable/SelectedUsersTransactionsTable.tsx
+++ src/components/molecules/SelectedUsersTransactionsTable/SelectedUsersTransactionsTable.tsx
@@ -55,7 +55,7 @@ const SelectedUsersTransactionsTable = ({
             scope="col"
             className="px-3 py-3.5 text-right pr-6 text-sm font-semibold text-gray-900 dark:text-slate-300"
           >
-            Select
+            <span className="sr-only">Select</span>
           </th>
         </tr>
       </thead>
--- src/components/organisms/SelectedUserReferralsSection/SelectedUserReferralsSection.tsx
+++ src/components/organisms/SelectedUserReferralsSection/SelectedUserReferralsSection.tsx
@@ -5,7 +5,7 @@ import { trpc } from 'utils/trpc';
 
 const SelectedUserReferralsSection = ({ user }: { user: User }) => {
   const [referrals, setReferrals] = useState<Referral[] | []>([]);
-  const { data, isLoading, error } = trpc.useQuery([
+  const { data, isLoading } = trpc.useQuery([
     'user.referralsByUserId',
     { userId: user.userID },
   ]);
@@ -17,17 +17,21 @@ const SelectedUserReferralsSection = ({ user }: { user: User }) => {
   }, [data]);
 
   return (
-    <div className="p-4 sm:px-6 lg:px-8">
-      <div className="sm:flex sm:items-center">
+    <div className="p-4 sm:px-6 lg:px-8 dark:bg-slate-700 rounded-xl">
+      <div className="sm:flex sm:items-center py-2">
         <div className="sm:flex-auto">
-          <h1 className="text-xl font-semibold text-gray-900">Referrals</h1>
-          <p className="mt-2 text-sm text-gray-700">{user.userID}</p>
+          <h1 className="text-xl font-semibold text-gray-900 dark:text-slate-100">
+            Referrals
+          </h1>
+          <p className="mt-2 text-sm text-gray-700 dark:text-slate-200">
+            {user.userID}
+          </p>
         </div>
       </div>
       <div className="mt-8 flex flex-col">
         <div className="-my-2 -mx-4 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
-            <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 md:rounded-lg">
+            <div className="overflow-hidden shadow ring-1 ring-black dark:ring-slate-600 ring-opacity-5 md:rounded-lg">
               {isLoading && (
                 <div className="flex items-center justify-center h-screen">
                   <Spinner />
