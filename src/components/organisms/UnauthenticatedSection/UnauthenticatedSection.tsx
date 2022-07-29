435fa91e5acaf1bd45dc49c872f3980f57146f69
sotomaque <sotomaque@gmail.com>
2022-07-28 20:40:24 -1000

    testing

--- src/components/organisms/UnauthenticatedSection/UnauthenticatedSection.tsx
+++ src/components/organisms/UnauthenticatedSection/UnauthenticatedSection.tsx
@@ -2,6 +2,7 @@
 import { useEffect } from 'react';
 import { useLayout } from 'store';
 import { signIn } from 'next-auth/react';
+import Image from 'next/image';
 
 const UnauthenticatedSection = () => {
   const { setShowLayout } = useLayout();
@@ -20,10 +21,13 @@ const UnauthenticatedSection = () => {
         <div className="flex-1 flex flex-col justify-center py-12 px-4 sm:px-6 lg:flex-none lg:px-20 xl:px-24">
           <div className="mx-auto w-full max-w-sm lg:w-96">
             <div>
-              <img
-                className="h-12 w-auto"
-                src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
-                alt="Workflow"
+              <Image
+                height="30"
+                width="90"
+                src={
+                  'https://eco.github.io/assets/images/eco-logo-color-140.png'
+                }
+                alt="Eco Admin"
               />
               <h2 className="mt-6 text-3xl font-extrabold text-gray-900">
                 Sign in to your account
@@ -67,7 +71,7 @@ const UnauthenticatedSection = () => {
         <div className="hidden lg:block relative w-0 h-screen flex-1">
           <img
             className="absolute inset-0 h-full w-full object-cover"
-            src="https://images.unsplash.com/photo-1505904267569-f02eaeb45a4c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1908&q=80"
+            src="/crypto.jpeg"
             alt=""
           />
         </div>
